4cf200e51ed64252fd575173d136eaa786cb00ad
Nozimjon Shukrullayev <nozimshukrullayev@gmail.com>
2025-03-30 15:39:08 +0500

    Api key corrected

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,7 +2,7 @@
 
 interface ImportMetaEnv {
 	readonly VITE_API_KEY: string
-	readonly VITE_GEMINI_VITE_API_KEY: string
+	readonly VITE_GEMINI_API_KEY: string
 	readonly VITE_SUPABASE_URL: string
 	readonly VITE_SUPABASE_ANON_KEY: string
 	// more env variables...
