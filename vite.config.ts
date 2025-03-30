b7ea2256225fa7c10d104365ba59f78d4be61f65
Nozimjon Shukrullayev <nozimshukrullayev@gmail.com>
2025-03-30 18:11:51 +0500

    corrected routing

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+	base: '/',
+	build: {
+		outDir: 'dist',
+	},
 	server: {
 		host: '::',
 		port: 8080,
